7d518f4f21f520535eb38c209b676f31950b64c2
inamdar nasiroddin <inamdarnasir8485@gmail.com>
2024-02-27 02:03:04 -0800

    add new cors origin policy

--- app.js
+++ app.js
@@ -14,12 +14,7 @@ const port=process.env.PORT||8000
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(cookieParser());
-app.use(cors({
-    origin: 'https://mern-project-1-c2u0.onrender.com', 
-    methods: ['GET', 'POST',"PUT","PATCH","DELETE"],      
-    allowedHeaders: ['Content-Type'], 
-    credentials: true             
-}));
+app.use(cors());
 Connect(process.env.URL,process.env.DBNAME)
 
 app.use("/user",userRouter);
